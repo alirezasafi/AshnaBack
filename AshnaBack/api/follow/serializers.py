74d95c0bea63c7dd01f685df772b89980a09c64e
aliraee1@yahoo.com <aliraee1@yahoo.com>
2019-07-08 18:08:50 +0430

    update serializer

--- AshnaBack/api/follow/serializers.py
+++ AshnaBack/api/follow/serializers.py
@@ -39,3 +39,34 @@ class FollowersSerializers(ModelSerializer):
             Followers.append(rel.Follower)
         Followers = PersonSerializers(Followers,many=True).data
         return Followers
+
+class CharitySerializers(ModelSerializer):
+    Image = SerializerMethodField()
+    class Meta:
+        model = Charity
+        fields = (
+            'id',
+            'Name',
+            'Image',
+        )
+    def get_Image(self,instance):
+        if not instance.Image:
+            return None
+        Image = "http://127.0.0.1:8000" + instance.Image.url
+        return Image
+    
+class FollowingsSerializers(ModelSerializer):
+    Followings = SerializerMethodField()
+    class Meta:
+        model = Person
+        fields = (
+            'Name',
+            'Followings'
+        )
+    def get_Followings(self,instance):
+        relations = Relation.objects.filter(Follower_id = instance.id)
+        Followings = []
+        for rel in relations:
+            Followings.append((rel.Follower))
+        Followings = CharitySerializers(Followings,many=True).data
+        return Followings
