b5f336536ae01f81f96d60753b3a902de5529035
aliraee1@yahoo.com <aliraee1@yahoo.com>
2019-07-08 18:02:38 +0430

    update some organization

--- AshnaBack/api/follow/views.py
+++ AshnaBack/api/follow/views.py
@@ -3,9 +3,12 @@ from rest_framework.generics import (
 )
 from Ashnabackapp.models import (
     Charity,
+    Person,
 )
-from .serializers import FollowersSerializers
-
+from .serializers import (
+    FollowersSerializers,
+    FollowingsSerializers
+)
 class FollowersView(ListAPIView):
     def get_queryset(self):
         Charity_obj = Charity.objects.filter(Charity_User_id=self.request.user.id)
@@ -16,3 +19,15 @@ class FollowersView(ListAPIView):
         Charity_obj = Charity.objects.filter(Charity_User_id=self.request.user.id)
         if Charity_obj.exists():
             return FollowersSerializers
+        
+        
+class FollowingsView(ListAPIView):
+    def get_queryset(self):
+        Person_obj = Person.objects.filter(Person_User_id=self.request.user.id)
+        if Person_obj.exists():
+            return Person.objects.filter(Name=Person_obj[0].Name)
+        
+    def get_serializer_class(self):
+        Person_obj = Person.objects.filter(Person_User_id=self.request.user.id)
+        if Person_obj.exists():
+            return FollowingsSerializers
