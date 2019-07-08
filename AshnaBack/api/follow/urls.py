8b4ca4c77ccce24f05288e5800de1fa174bae0bf
aliraee1@yahoo.com <aliraee1@yahoo.com>
2019-07-08 20:06:44 +0430

    update make follow url

--- AshnaBack/api/follow/urls.py
+++ AshnaBack/api/follow/urls.py
@@ -2,9 +2,12 @@ from django.urls import path, include
 from .views import (
     FollowersView,
     FollowingsView,
+    PersonFollowCharity
 )
 
 urlpatterns = [
     path('followers/',FollowersView.as_view(),name='get_Followers'),
-    path('followings/',FollowingsView.as_view(),name='get_Followings')
+    path('followings/',FollowingsView.as_view(),name='get_Followings'),
+    path('',PersonFollowCharity.as_view(),name='personFollowCharity'),
+
 ]
