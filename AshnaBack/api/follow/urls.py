21294abbbbe5ec777c6404db9b2981b07b5bbc29
aliraee1@yahoo.com <aliraee1@yahoo.com>
2019-07-08 18:10:16 +0430

    upate in importing objs

--- AshnaBack/api/follow/urls.py
+++ AshnaBack/api/follow/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path, include
-from .views import FollowersView
+from .views import (
+    FollowersView,
+    FollowingsView,
+)
 
 urlpatterns = [
     path('followers/',FollowersView.as_view(),name='get_Followers'),
+    path('followings/',FollowingsView.as_view(),name='get_Followings')
 ]
