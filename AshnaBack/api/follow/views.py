a543d189e0e9f1107b71979a7b25bcad8f7aa431
aliraee1@yahoo.com <aliraee1@yahoo.com>
2019-07-08 20:17:16 +0430

    update view

--- AshnaBack/api/follow/views.py
+++ AshnaBack/api/follow/views.py
@@ -1,14 +1,31 @@
 from rest_framework.generics import (
-    ListAPIView
+    ListAPIView,
+    
 )
 from Ashnabackapp.models import (
     Charity,
     Person,
+    Relation
 )
 from .serializers import (
     FollowersSerializers,
-    FollowingsSerializers
+    FollowingsSerializers,
+    PersonFollowCharotySerializer,
+
 )
+from rest_framework.views import APIView
+from rest_framework.permissions import AllowAny
+from rest_framework.status import (
+    HTTP_404_NOT_FOUND,
+    HTTP_201_CREATED,
+    HTTP_200_OK,
+    HTTP_400_BAD_REQUEST
+)
+from rest_framework.response import Response
+
+from django.contrib.auth import get_user_model
+from django.contrib.auth import authenticate
+
 class FollowersView(ListAPIView):
     def get_queryset(self):
         Charity_obj = Charity.objects.filter(Charity_User_id=self.request.user.id)
@@ -31,3 +48,35 @@ class FollowingsView(ListAPIView):
         Person_obj = Person.objects.filter(Person_User_id=self.request.user.id)
         if Person_obj.exists():
             return FollowingsSerializers
+user_model=get_user_model()
+class PersonFollowCharity(APIView):
+    serializer_class=PersonFollowCharotySerializer
+    permission_class=[AllowAny]
+    def post(self,request,*args,**kwargs):
+        data=request.data
+        # print(data)
+        # print(request.data['Follower'])
+        response={'Error':''}
+        person_id=data['Follower']
+        charity_id=data['Followed']
+        person_obj=Person.objects.filter(id=person_id)[0]
+        charity_obj=Charity.objects.filter(id=charity_id)[0]
+        if not person_obj:
+            response['Error']='Person Does Not Enterd.'
+        elif not charity_obj:
+            response['Error']='Charity Does Not Enterd.'
+        else:
+            follow=Relation(
+                Follower=person_obj,
+                Followed=charity_obj
+            )
+            if follow not in Relation.objects.all():
+                follow.save()
+                return Response(response,HTTP_200_OK)
+            else:
+                # TODO:fix bug in following
+                response['Error']='Relation has been added.'
+                
+            
+               
+        return Response(response,HTTP_400_BAD_REQUEST)
